c8929cade2416ef4434b3c3f9bdef64731b180de
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-16 11:12:23 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Katelyn Curtiss
+# Setember 16 2024
+# Mad Libs Practice
+
+noun = input("Noun: ")
+verb = input("Verb: ")
+verb1 = input("Verb: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+noun5 = input("Noun: ")
+noun6 = input("Noun: ")
+
+madlibs = f"Hey! Welcome {noun} . Here, we love to make {verb} an {noun2} . Walk down {noun3} to the {noun4} for a cup of {noun5} where you can {verb1} along the {noun6}. " 
+
+print(madlibs)
